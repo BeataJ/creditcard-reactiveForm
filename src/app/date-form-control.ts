e6275b163c414b557908e49e80eafa68df46815e
beataj <beata@emc2wave.com>
2021-03-18 21:20:42 -0700

    add super.setValue(value, options)

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -3,5 +3,6 @@ import { FormControl } from '@angular/forms';
 export class DateFormControl extends FormControl{
     setValue(value: string, options: any) {
         console.log(value);
+        super.setValue(value, options)
     }
 }
